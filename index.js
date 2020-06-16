f02ccf9d4a073c5e6959ff97becc95bb79199b39
konst <konstantin.mikheev@icord.cz>
2020-06-16 15:15:54 +0200

    port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = parseInt(process.env.PORT || '3000', 10);
 require('dotenv').config();
 const { vKAuthFirstStep, vkLoginComplete } = require('./vk-auth');
 
