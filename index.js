446982fce33c544573f32c74f7ecb06a5f26218b
konst <konstantin.mikheev@icord.cz>
2020-06-16 15:11:48 +0200

    sleep

--- index.js
+++ index.js
@@ -7,6 +7,18 @@ const { vKAuthFirstStep, vkLoginComplete } = require('./vk-auth');
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.get('/login/vk', (req, res) => vKAuthFirstStep(res));
+app.get('/test', async (req, res) => {
+  console.info('before sleep');
+  const reds = await new Promise(resolve => {
+    setTimeout(() => {
+      console.info('SLEPT WELL');
+      resolve('GTFO');
+    }, 10000);
+  });
+  console.info('after sleep');
+
+  return res.send(reds);
+});
 app.get('/login/vk/complete', vkLoginComplete);
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
