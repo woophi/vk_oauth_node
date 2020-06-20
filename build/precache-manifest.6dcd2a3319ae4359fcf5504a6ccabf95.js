cf16eb5fadcf2e1fb330c89cc2b9230178f05312
konst <konstantin.mikheev@icord.cz>
2020-06-20 22:11:02 +0200

    deploying a secret build

---
+++ build/precache-manifest.6dcd2a3319ae4359fcf5504a6ccabf95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d87cf9799b798e6fecdd104715dc69b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dec7f79c8494a17c5fae",
+    "url": "/static/css/2.1cb049e5.chunk.css"
+  },
+  {
+    "revision": "dec7f79c8494a17c5fae",
+    "url": "/static/js/2.5f9f31cf.chunk.js"
+  },
+  {
+    "revision": "4d9448e5a1b29056c618b2c8fc1e0930",
+    "url": "/static/js/2.5f9f31cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef8ae6da065da079fb4f",
+    "url": "/static/js/main.ea5b8117.chunk.js"
+  },
+  {
+    "revision": "d5d50483e52d53a4b219",
+    "url": "/static/js/runtime-main.29d8161a.js"
+  }
+]);
--- index.js
+++ index.js
@@ -1,24 +1,22 @@
 const express = require('express');
 const app = express();
-const port = parseInt(process.env.PORT || '3000', 10);
+const port = parseInt(process.env.PORT || '3005', 10);
+const exphbs = require('express-handlebars');
+
 require('dotenv').config();
 const { vKAuthFirstStep, vkLoginComplete } = require('./vk-auth');
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+app.use(express.static('build'));
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.get('/login/vk', (req, res) => vKAuthFirstStep(res));
-app.get('/test', async (req, res) => {
-  console.info('before sleep');
-  const reds = await new Promise(resolve => {
-    setTimeout(() => {
-      console.info('SLEPT WELL');
-      resolve('GTFO');
-    }, 10000);
-  });
-  console.info('after sleep');
-
-  return res.send(reds);
-});
 app.get('/login/vk/complete', vkLoginComplete);
+
+
+app.get('/app', (req, res) => {
+  res.render('home');
+});
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
